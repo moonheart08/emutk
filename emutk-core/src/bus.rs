64162b3369da9d85ff6b8a1eb0616da63c7f0316
moonheart08 <moonheart08@users.noreply.github.com>
2020-03-19 00:53:45 -0500

    further improvements to core, add big endian test bus.

--- emutk-core/src/bus.rs
+++ emutk-core/src/bus.rs
@@ -11,11 +11,11 @@ pub trait Bus<Err> {
     fn write<T: ByteRepr>(&mut self, addr: usize, data: T) -> Result<(), Err>;
 
     /// Read a ByteRepr implementing value from the bus at the selected address, with a tag.
-    fn read_tagged<T: ByteRepr>(&mut self, addr: usize, tag: Self::Tag) -> Result<T, Err> {
+    fn read_tagged<T: ByteRepr>(&mut self, addr: usize, _tag: Self::Tag) -> Result<T, Err> {
         self.read(addr)
     }
     /// Write a ByteRepr implementing value to the bus at the selected address, with a tag.
-    fn write_tagged<T: ByteRepr>(&mut self, addr: usize, data: T, tag: Self::Tag) -> Result<(), Err> {
+    fn write_tagged<T: ByteRepr>(&mut self, addr: usize, data: T, _tag: Self::Tag) -> Result<(), Err> {
         self.write(addr, data)
     }
 }
@@ -37,7 +37,84 @@ impl BusErrorCommon for SimpleBusError {
     }
 }
 
-/// Very simple bus with 32KiB of RAM and 32KiB of ROM, ment for testing.
+/// Very simple little endian bus with 32KiB of RAM and 32KiB of ROM, ment for testing.
+pub struct RAMROMBusBE<Err> {
+    ram: Vec<u8>,
+    rom: Vec<u8>,
+    __err: std::marker::PhantomData<Err>,
+}
+
+impl<E> RAMROMBusBE<E> {
+    pub fn new<Err>() -> RAMROMBusBE<Err> {
+        RAMROMBusBE {
+            ram: vec![0; 32768],
+            rom: vec![0; 32768],
+            __err: PhantomData,
+        }
+    }
+}
+
+impl<E: BusErrorCommon> Bus<E> for RAMROMBusBE<E> {
+    type Tag = ();
+
+    fn read<T: ByteRepr>(&mut self, addr: usize) -> Result<T, E> {
+        let len = T::byte_len();
+
+        if addr + len > u16::MAX as usize {
+            return Err(E::out_of_bounds());
+        } else {
+            let v: T = match addr {
+                n if n < 32768 - len => {
+                    let s = &self.ram[addr..len];
+                    T::from_be_bytes(s)
+                } 
+                n if n > 32768 => {
+                    let s = &self.rom[addr..len];
+                    T::from_be_bytes(s)
+                }
+                _ => {
+                    let x = &self.ram[addr..32768];
+                    let y = &self.rom[0..(len - x.len())];
+                    let f = [x, y].concat();
+                    T::from_be_bytes(&f)
+                }
+            };
+            return Ok(v);
+        }
+    }
+
+    fn write<T: ByteRepr>(&mut self, addr: usize, data: T) -> Result<(), E> {
+        let len = T::byte_len();
+        if addr + len > u16::MAX as usize {
+            return Err(E::out_of_bounds());
+        } else {
+            match addr {
+                n if n < 32768 - len => {
+                    let s = &mut self.ram[addr..len];
+                    data.copy_into_be_bytes( s);
+                } 
+                n if n > 32768 => {
+                    let s = &mut self.rom[addr..len];
+                    data.copy_into_be_bytes(s);
+                }
+                _ => {
+                    let x = &mut self.ram[addr..32768];
+                    let y = &mut self.rom[0..(len - x.len())];
+                    
+                    let mut v = vec![0; len];
+                    data.copy_into_be_bytes(&mut v);
+
+                    x.copy_from_slice(&v[0..x.len()]);
+                    y.copy_from_slice(&v[x.len()..v.len()]);
+                }
+            };
+            return Ok(());
+        }
+    }
+}
+
+
+/// Very simple little endian bus with 32KiB of RAM and 32KiB of ROM, ment for testing.
 pub struct RAMROMBusLE<Err> {
     ram: Vec<u8>,
     rom: Vec<u8>,
@@ -113,6 +190,7 @@ impl<E: BusErrorCommon> Bus<E> for RAMROMBusLE<E> {
     }
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
     #[test]
